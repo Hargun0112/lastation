52444de7df8f2d7c1f29eab2cc74ea4afc5ee7b9
Hargun Singh <hargun0112@gmail.com>
2023-06-18 12:43:30 +0530

    Fixing on first template

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,19 @@
 $(document).ready(
-    $("#options").change(function(){
+    $("#options").change(function(e){
         var id=$(this).val();
         $(".tierItem").hide();
         $(`.tierItem[data-id='${id}']`).show();
-        $(`.payButton`).addClass("d-none");
-        $(`.payButton[data-sub-id=${id}`).removeClass("d-none");
+        var plan=$("#options").find("option:selected").attr("data-planid");
+        if(plan===undefined)
+        {
+            $(".payButton").addClass("d-none");
+            $(".checkoutButton").removeClass("d-none");
+        }
+        else
+        {
+            planId=plan;
+            $(".checkoutButton").addClass("d-none");
+            $(`.payButton`).removeClass("d-none");
+        }
     })
 )
