b868322624f6b9d5dcaf59e942ec34772d203624
Hargun Singh <hargun0112@gmail.com>
2023-07-10 08:37:51 +0530

    role added

--- assets/js/main2.js
+++ assets/js/main2.js
@@ -1,5 +1,11 @@
+let tier=$("input['name='tier']:checked").val();
+let email=$("#email");
+let discordUsername=$("#discordUser");
+let vrchatusername=$("#vrchatusername");
+
 $(document).ready(function(){
     $("input[name='tier']").change(function(){
         planId=$(this).attr("data-planid");
+        tier=$(this).val();
     })
 });
